57fa226b9ca297df088bfa6d0f59504a25a20eaf
jhonfredy36 <127421007+jhonfredy36@users.noreply.github.com>
2023-07-27 01:12:22 -0500

    *Cambios en admin

--- src/components/layouts/Admin/Administrador.jsx
+++ src/components/layouts/Admin/Administrador.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import ReactPlayer from 'react-player';
+import VistaCliente from '../VistaCliente';
 
 
 const Administrador = () => {
   return (
+    <section>
     <header className='headerAdmin'>
         <h1>Administrador</h1>
         <button>Estado financiero</button>
         <button>Empleados</button>
         <button>Salas</button>
         <Link className='cerrar' to={'/Cerrar'}>Cerrar sesion</Link>
-    </header>
+    </header>  
+    <VistaCliente />
+    <button>Crear</button>
+    <button>Editar</button>
+    </section>
+    
   )
 }
 
